09bb2344a266415319ed557eceb7de9101447ff7
IAmSilK <iamsilk3495@gmail.com>
2023-02-22 12:45:38 -0400

    Simplify FakeClient code

--- FrogAlert.FakeClient/Program.cs
+++ FrogAlert.FakeClient/Program.cs
@@ -4,9 +4,9 @@ client.BaseAddress = new Uri("http://localhost:5003/");
 var environmentSnapshots = new List<(int Delay, float Humidity, float TempC)>
 {
     (5, 70.2f, 22.2f),
-    (0, 73.2f, 22.6f),
-    (0, 77.2f, 23.6f),
-    (0, 79.2f, 23.9f),
+    (20, 73.2f, 22.6f),
+    (20, 77.2f, 23.6f),
+    (20, 79.2f, 23.9f),
     (20, 78.1f, 24.2f),
     (20, 78.1f, 24.5f),
     (20, 78.1f, 23.2f)
@@ -15,15 +15,14 @@ var environmentSnapshots = new List<(int Delay, float Humidity, float TempC)>
 foreach (var (delay, humidity, tempC) in environmentSnapshots)
 {
     await Task.Delay(delay * 1000);
-    
-    var parameters = new Dictionary<string, string>()
-    {
-        { "tempC", tempC.ToString("#.#") },
-        { "humidity", humidity.ToString("#.#") },
-        { "time", DateTimeOffset.UtcNow.AddSeconds(-15).ToString("yyyy-MM-ddTHH:mm:ss.fffZ") }
-    };
 
-    var urlParameters = string.Join("&", parameters.Select(pair => $"{pair.Key}={Uri.EscapeDataString(pair.Value)}"));
+    var tempCStr = tempC.ToString("#.#");
+    var humidityStr = humidity.ToString("#.#");
+    var timeStr = DateTimeOffset.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+
+    var urlParameters = $"tempC={Uri.EscapeDataString(tempCStr)}" +
+                        $"&humidity={Uri.EscapeDataString(humidityStr)}" +
+                        $"&time={Uri.EscapeDataString(timeStr)}";
 
     Console.WriteLine($"Posting data. Humidity: {humidity:#.#}%, Temperature: {tempC:#.#} C");
 
